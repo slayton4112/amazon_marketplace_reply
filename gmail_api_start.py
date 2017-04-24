9809a9c9b501256874cf8edf69eb9183f64fe34d
Scott Layton <Scott Layton>
2017-04-23 22:54:02 -0400

    initial commit

---
+++ gmail_api_start.py
@@ -0,0 +1,178 @@
+from __future__ import print_function
+import httplib2
+import os
+import json
+from apiclient import discovery
+from apiclient import errors
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/gmail-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+CLIENT_SECRET_FILE = 'gmail_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~\Documents')
+    print(home_dir)
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'client_secret.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def ListMessagesMatchingQuery(service, user_id, query=''):
+    """List all Messages of the user's mailbox matching the query.
+
+    Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    query: String used to filter messages returned.
+    Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
+
+    Returns:
+    List of Messages that match the criteria of the query. Note that the
+    returned list contains Message IDs, you must use get with the
+    appropriate ID to get the details of a Message.
+    """
+    try:
+        response = service.users().messages().list(userId=user_id,
+                                   q=query).execute()
+        messages = []
+        if 'messages' in response:
+            messages.extend(response['messages'])
+
+        while 'nextPageToken' in response:
+            page_token = response['nextPageToken']
+            response = service.users().messages().list(userId=user_id, q=query,
+                             pageToken=page_token).execute()
+            messages.extend(response['messages'])
+
+        return messages
+
+    except errors.HttpError, error:
+        print('An error occurred: %s' % error)
+
+def GetMessage(service, user_id, msg_id):
+    """Get a Message with given ID.
+
+    Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: The ID of the Message required.
+
+    Returns:
+    A Message.
+    """
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+
+        # print('Message snippet: %s' % message['snippet'].encode("utf-8"))
+
+        return message
+    except errors.HttpError, error:
+        print(unicode('An error occurred: %s' % error),'utf-8')
+
+def send_message(service, user_id, message):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print('Message Id: %s' % message['id'])
+    return message
+  except errors.HttpError, error:
+    print('An error occurred: %s' % error)
+
+def create_message(sender, to, subject, message_text):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject # same as subject you respond to
+  message['threadId'] = 'id responding to'
+  return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    labels = ListMessagesMatchingQuery(service = service, user_id = 'me', query = '*@marketplace.amazon.com')
+
+    messages = {}
+    for i in labels: 
+        message = GetMessage(service,'me',i['id'])
+        #print(message)
+        for header in message['payload']['headers']:
+            #print(header['name'])
+            if header['name'] == 'Subject':
+                print('Message Subject: %s' % header['value'].encode("utf-8"))
+                subject = header['value']
+            if header['name'] == 'Reply-To':
+                print('Message Reply-To: %s' % header['value'].encode("utf-8"))
+                subject = header['value']
+        print(message['threadId'])
+
+        
+
+    #print(json.dumps(messages,sort_keys=True,indent=4))
+
+if __name__ == '__main__':
+    main()
